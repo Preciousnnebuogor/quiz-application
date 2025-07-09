3207a74871175a2bc6675efdc381f86723a76800
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2025-07-08 23:53:28 -0700

    updated

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,58 @@
 export default function Navbar() {
-  <style>
-
-  </style>
   return (
-    <div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignContent: "start",
-          padding: "10px",
-        }}
-      >
+    <div className={"nav-body"}>
+      <div className="nav-container">
         <img
-          src="/assets/icon.jpeg"
+          src="/assets/icon.png"
           alt="Navbar Icon"
-          style={{ width: "100px" }}
-        />
-        <div
           style={{
-            display: "flex",
-            gap: "20px", // spacing between links
-            alignItems: "center",
-            padding: "10px",
+            width: "100px",
           }}
-        >
+        />
+        <div className="nav-content">
           <a href="">Home</a>
           <a href="">About</a>
           <a href="">Features</a>
         </div>
 
-        <div>
-          <a href="">Login</a>
-          <a href="">Take a Quiz</a>
+        <div className="nav-content2"
+          style={{
+            display: "flex",
+            gap: "20px",
+            alignItems: "center",
+          }}
+        >
+          <a
+            href=""
+            style={{
+              color: "white",
+              textDecoration: "none",
+              fontWeight: "bold",
+            }}
+          >
+            Login
+          </a>
+          <a
+            href=""
+            style={{
+              color: "white",
+              textDecoration: "none",
+              fontWeight: "bold",
+            }}
+          >
+            <button
+              style={{
+                border: "1px solid white",
+                padding: "5px",
+                background: "white",
+                color: "#FF6B6B",
+                fontWeight: "bold",
+                cursor: "pointer",
+              }}
+            >
+              Take a quiz
+            </button>
+          </a>
         </div>
       </div>
     </div>
